75d9a1f123fce6e20c839f7b87eb985f97ad2abe
윤진환 <www139455@naver.com>
2026-03-27 13:37:10 +0900

    upload

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody2D rigidbody2;
+    float jumbForce = 680.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Application.targetFrameRate = 60;
+        this.rigidbody2 = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space) {
+            this.rigidbody2.AddForce(transform.up * this.jumbForce);
+        }
+    }
+}
