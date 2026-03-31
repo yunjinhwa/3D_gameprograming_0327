8f509c1705371521038a4da805f4c551ec298783
윤진환 <www139455@naver.com>
2026-03-31 23:34:55 +0900

    fix

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -14,7 +14,7 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space) {
+        if(Input.GetKeyDown(KeyCode.Space)) {
             this.rigidbody2.AddForce(transform.up * this.jumbForce);
         }
     }
